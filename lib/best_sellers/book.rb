a74507409258b0935a10c3d2e228333ede6853dc
Pia Kim <pkim2320@gmail.com>
2017-02-07 23:46:40 +0000

    Changed self.today method to include scrape

--- lib/best_sellers/book.rb
+++ lib/best_sellers/book.rb
@@ -34,7 +34,20 @@ class BestSellers::Book
     # [book_1, book_2]
 
     #scrape site and return books
-    self.scrape_books
+    doc = Nokogiri::HTML(open("http://www.barnesandnoble.com/b/the-new-york-times-bestsellers-trade-paperback-fiction/_/N-1p3v"))
+
+    book_1 = self.new
+      book_1.title = doc.search("p.product-info-title a")[0].text
+      book_1.author = doc.css("span.contributors a")[0].text.strip
+      book_1.published_date = doc.css("span.publ-date")[0].text.gsub("(", "").gsub(")", "").strip
+
+      book_2 = self.new
+        book_2.title = doc.search("p.product-info-title a")[1].text
+        book_2.author = doc.css("span.contributors a")[1].text.strip
+        book_2.published_date = doc.css("span.publ-date")[1].text.gsub("(", "").gsub(")", "").strip
+
+        [book_1, book_2]
+    # self.scrape_books
   end
 
   def self.scrape_books
@@ -46,26 +59,26 @@ class BestSellers::Book
     books
   end
 
-  def self.scrape_nytimes
-
-
-doc = Nokogiri::HTML(open("http://www.barnesandnoble.com/b/the-new-york-times-bestsellers-trade-paperback-fiction/_/N-1p3v"))
-
-book_1 = self.new
-  book_1.title = doc.search("p.product-info-title a")[0].text
-  book_1.author = doc.css("span.contributors a")[0].text.strip
-  book_1.published_date = doc.css("span.publ-date")[0].text.gsub("(", "").gsub(")", "").strip
-
-  book_2 = self.new
-    book_2.title = doc.search("p.product-info-title a")[1].text
-    book_2.author = doc.css("span.contributors a")[1].text.strip
-    book_2.published_date = doc.css("span.publ-date")[1].text.gsub("(", "").gsub(")", "").strip
-
+#   def self.scrape_nytimes
+#
+#
+# doc = Nokogiri::HTML(open("http://www.barnesandnoble.com/b/the-new-york-times-bestsellers-trade-paperback-fiction/_/N-1p3v"))
+#
+# book_1 = self.new
+#   book_1.title = doc.search("p.product-info-title a")[0].text
+#   book_1.author = doc.css("span.contributors a")[0].text.strip
+#   book_1.published_date = doc.css("span.publ-date")[0].text.gsub("(", "").gsub(")", "").strip
+#
+#   book_2 = self.new
+#     book_2.title = doc.search("p.product-info-title a")[1].text
+#     book_2.author = doc.css("span.contributors a")[1].text.strip
+#     book_2.published_date = doc.css("span.publ-date")[1].text.gsub("(", "").gsub(")", "").strip
+#
 
 
 # doc.css("ul.record").collect do |info|
 #   book_info = {
-#   :title => 
+#   :title =>
 # }
 # book_info
 # end
